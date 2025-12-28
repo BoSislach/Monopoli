a0852f7027d9485d2e0cc0596d61ec164b9486bc
pene <nilton18like@gmail.com>
2025-12-28 18:40:59 +0100

    Update Casella.java

--- Casella.java
+++ Casella.java
@@ -1,11 +1,26 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 class Casella {
     protected String nome;
-    protected int posizione;
-    
-    public Casella(String nome, int posizione) {
+    protected Casella precedente;
+    protected Casella successiva;
+    protected int x;
+    protected int y;
+
+    public Casella(String nome,int x,int y) {
         this.nome = nome;
-        this.posizione = posizione;
+        this.x = x;
+        this.y = y;
     }
-    
-}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
