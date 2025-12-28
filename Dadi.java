1cd4022d2272e3372bd88efe6c365745f8d3e5a8
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-12-28 18:31:40 +0100

    Update Dadi.java

--- Dadi.java
+++ Dadi.java
@@ -2,5 +2,13 @@ class Dadi {
     protected int dado1;
     protected int dado2;
 
-    
+    public void lanciaDadi() {
+        dado1 = (int) (Math.random() * 6) + 1;
+        dado2 = (int) (Math.random() * 6) + 1;
+        
+    }
+
+    public int getSomma() {
+        return dado1 + dado2;
+    }
 }
