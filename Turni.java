3efe863b1cf9cc970867f682ffb2333ff571f6cf
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2026-01-30 01:18:28 +0100

    Update Turni.java

--- Turni.java
+++ Turni.java
@@ -4,21 +4,23 @@ public class Turni {
     private final ArrayList<Giocatore> giocatori;
     private int turnoCorrente;
 
-
     public Turni(ArrayList<Giocatore> giocatori) {
         this.giocatori = giocatori;
         this.turnoCorrente = 0;
     }
 
     public Giocatore getGiocatoreCorrente() {
-    if (turnoCorrente >= giocatori.size()) {
-        turnoCorrente = 0;
+        if (giocatori.isEmpty()) {
+            return null;
+        }
+        if (turnoCorrente >= giocatori.size()) {
+            turnoCorrente = 0;
+        }
+        return giocatori.get(turnoCorrente);
     }
-    return giocatori.get(turnoCorrente);
-}
 
     public void passaAlProssimoTurno() {
-       if (turnoCorrente < giocatori.size() - 1) {
+        if (turnoCorrente < giocatori.size() - 1) {
             turnoCorrente++;
         } else {
             turnoCorrente = 0;
